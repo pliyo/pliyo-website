62908d2c08824568ea7157dea8adb9da2172d6eb
Juanjo Cerezuela <juanjo.cerezuela@guestline.com>
2020-05-23 00:32:10 +0100

    Adding pliyo image with coney island as background

--- ClientApp/src/components/Bio.js
+++ ClientApp/src/components/Bio.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import pliyo from '../images/pliyo.jpg';
 
 const Bio = (props) => {
   return (
     <div>
       <h1>Who is Juanjo Cerezuela?</h1>
+      <img
+        src={pliyo}
+        alt="pliyo wearing a cowboy hat with coney island as a background"
+      />
       <p>
         Juan Jose Guerrero Cerezuela, for short Juanjo Cerezuela, is a spaniard
         living in the UK
