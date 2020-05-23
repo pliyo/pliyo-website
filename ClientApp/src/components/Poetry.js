18076248d92bff4b23507860de5caf4d140cf9b6
Juanjo Cerezuela <juanjo.cerezuela@guestline.com>
2020-05-23 22:36:05 +0100

    Saving poetry first draft and shorinji first draft

--- ClientApp/src/components/Poetry.js
+++ ClientApp/src/components/Poetry.js
@@ -1,10 +1,133 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import LaLuz from '../images/laluz.png';
+import LaVida from '../images/lavidapordelante.png';
+import PliyoSenpai from '../images/pliyosenpai.jpg';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+export function LaVidaPorDelante() {
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={LaVida}
+          title="La vida por delante"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            La vida por delante
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Obra antológica que recoge 60 poetas andaluces nacidos a partir de
+            1980, dirigida pro Ana Alvea Sánchez y Jorge Díaz Martínez.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          <a href="https://www.edicionesenhuida.es/producto/lvpd/">
+            Learn More
+          </a>
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+export function Banderin() {
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={PliyoSenpai}
+          title="Banderines del Zaguan"
+          alt="Banderines del Zaguan"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            La luz más bella
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Yo invoco la tormenta
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          <a href="http://losbanderines.blogspot.com/search?updated-max=2013-01-28T13:58:00-08:00&max-results=499&start=17&by-date=false">
+            Learn More
+          </a>
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+export function LaLuzMasBella() {
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={LaLuz}
+          title="La luz mas bella"
+          alt="La luz mas bella"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            La luz más bella
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Antología de jóvenes poetas almerienses. Coordinada por Martina
+            Mateo Jiménez y Daniel J. García López.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          <a href="https://www.edicionesenhuida.es/producto/la-luz-mas-bella/">
+            Learn More
+          </a>
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
 
 const Poetry = (props) => {
   return (
     <div>
-      <h1>Poetry is not dead</h1>
-      <p>I have participated in some anthologies...</p>
+      <h1>Poetry</h1>
+      <p>
+        When I was back in Spain I used to write poetry in spanish.
+        <br />I still do, but I'm not planning on writing any book any time
+        soon. These are some of the books I have participated in:
+        I also wrote poems for "Antologia tras las rejas"
+      </p>
+      <LaLuzMasBella></LaLuzMasBella>
+      <br />
+      <LaVidaPorDelante></LaVidaPorDelante>
+      <br />
+      <Banderin></Banderin>
     </div>
   );
 };
--- ClientApp/src/components/Shorinji.js
+++ ClientApp/src/components/Shorinji.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import ShorinjiKempo from '../images/rsz_pliyoshorinjikyoto.jpg';
 
 const Shorinji = (props) => {
   return (
     <div>
-      <h1>Shorinji Kempo practice</h1>
-      <p>SHORINJI KEMPO!</p>
+      <h1>Shorinji Kempo</h1>
+      <p>I have been practising for 14 years</p>
+      <img
+        src={ShorinjiKempo}
+        alt="Juanjo in Rakuto Doin practising Shorinji Kempo"
+      />
+      ;
     </div>
   );
 };
